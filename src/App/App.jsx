90be46e493f5c88f1f9445dcc0db879ec4dbc13e
Phan Tran Duy An <duyan@MacBook-Air-cua-Phan.local>
2021-08-11 18:40:15 +0700

    Update Router

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import Routes from "../routes/Routes";
-import Header from "../components/Header";
-import { BrowserRouter as Router } from "react-router-dom";
-import Footer from "../components/Footer";
-import firebase from "firebase";
+import React from 'react';
+import Routes from '../routes/Routes';
+import Header from '../components/Header';
+import { BrowserRouter as Router, HashRouter } from 'react-router-dom';
+import Footer from '../components/Footer';
+import firebase from 'firebase';
 
 const App = () => {
   // Configure Firebase.
   const config = {
     apiKey: process.env.REACT_APP_FIREBASE_API,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN
   };
   if (!firebase.apps.length) {
     firebase.initializeApp(config);
@@ -18,11 +18,11 @@ const App = () => {
   }
 
   return (
-    <Router>
+    <HashRouter>
       <Header />
       <Routes />
       <Footer />
-    </Router>
+    </HashRouter>
   );
 };
 
